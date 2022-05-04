ea808e30ae6cb7c8b18f115148f4b3d61891bce8
Cesar Encarnacion <cesar_encarnacion@claro.com.do>
2022-05-03 21:05:45 -0400

    Prevent duplicate mails in use

--- BackEnd/Persons.Tests/PersonsTest.cs
+++ BackEnd/Persons.Tests/PersonsTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using PersonsApi.Models;
+using System;
 using System.Linq;
 
 namespace Persons.Tests
@@ -10,6 +11,7 @@ namespace Persons.Tests
         public void GetPersons_IsNotEmpty()
         {
             var service = Utils.Helper.GetPersonService();
+            
             Assert.IsNotEmpty(service.GetAll());
         }
 
@@ -18,6 +20,7 @@ namespace Persons.Tests
         {
             var person = new Person { Name = "Juan", SurName = "ele", Mail = "Test@gmail.com" };
             var service = Utils.Helper.GetPersonService();
+            
             service.Add(person);
 
             Assert.IsTrue(service.GetAll().Contains(person));
@@ -26,11 +29,25 @@ namespace Persons.Tests
         public void DeletePersons_IsDeletingAll()
         {
             var service = Utils.Helper.GetPersonService();
+
             foreach (var person in service.GetAll())
             {
                 service.Delete(person.Id);
             }
+
             Assert.IsEmpty(service.GetAll());
+        }
+
+        [Test]
+        public void DuplicateEmailInPersons_IsThrowingException()
+        {
+            var person = new Person { Name = "Juan", SurName = "Doe", Mail = "TestDoe@gmail.com" };
+            var personWithDuplicate = new Person { Name = "Pedro", SurName = "Doe", Mail = "TestDoe@gmail.com" };
+            var service = Utils.Helper.GetPersonService();
+
+            service.Add(person);
+
+            Assert.ThrowsAsync<Exception>(() => service.Add(personWithDuplicate));
         }
 
     }
--- BackEnd/PersonsApi/Implementations/Services/PersonsService.cs
+++ BackEnd/PersonsApi/Implementations/Services/PersonsService.cs
@@ -11,6 +11,8 @@ namespace PersonsApi.Implementations.Services
 
         public async Task Add(Person newPerson)
         {
+            var mailExist = _context.Persons.Any(x => x.Mail == newPerson.Mail);
+            if (mailExist) throw new Exception("Mail already in use");
             await _context.Persons.AddAsync(newPerson);
             await _context.SaveChangesAsync();
         }
@@ -18,7 +20,7 @@ namespace PersonsApi.Implementations.Services
         public async Task Delete(int id)
         {
             var person = await _context.Persons.FindAsync(id);
-            if (person == null) throw new KeyNotFoundException();
+            if (person == null) throw new Exception("User not Found");
 
             _context.Persons.Remove(person);
             await _context.SaveChangesAsync();
